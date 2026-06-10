4998f99d81a17aa9b4780ac3d1d322bbcc2c7ee9
Benjamin <bemya001@osloskolen.no>
2026-06-10 13:53:22 +0200

    front page started

--- filmsite/movies/models.py
+++ filmsite/movies/models.py
@@ -1,3 +1,12 @@
 from django.db import models
 
-# Create your models here.
+class Movie(models.Model):
+    title = models.CharField(max_length=200)
+    genre = models.CharField(max_length=100)
+    year = models.IntegerField()
+    director = models.CharField(max_length=200)
+    description = models.TextField()
+    rating = models.DecimalField(max_digits=3, decimal_places=1)
+
+    def __str__(self):
+        return f"{self.title} ({self.year})"
